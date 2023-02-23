50f5fd0068880b24e7daf77259cbe2180f72db30
Elo <elianadnavarro@gmail.com>
2023-02-23 16:56:59 -0300

    semana uno

--- 01-introduccion/09ejercicio.py
+++ 01-introduccion/09ejercicio.py
@@ -1,3 +1,11 @@
 # ## Ejercicio 9:
 
 # Escriba un programa que lea input del usuario: cantidad de días, horas, minutos y segundos y que en base a esos datos, calcule el total en segundos.
+dias = int(input('dias: '))
+horas = int(input('horas: '))
+minutos = int(input('minutos: '))
+segundos = int(input('dias: '))
+
+totalseg = (dias * 24 * 60 * 60) + (horas * 60 * 60) + minutos * 60 + segundos
+
+print(f'el total en segundos es {totalseg}')
